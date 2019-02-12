30ca015e79503bf694579bcd37d888509f45f37f
Kubenic <jordan.venant@gmail.com>
2019-02-13 00:12:00 +0100

    c'est pas ouf

--- tools/importer/importer.js
+++ tools/importer/importer.js
@@ -1,9 +1,34 @@
 const term = require('terminal-kit').terminal;
+const mongoose = require('mongoose');
+const dummy = require('mongoose-dummy');
+//const db = require('./application/libs/db');
+const API_BASE_URL = "https://www.dealabs.com/rest_api/v2/";
+const API_DEAL_HOTTEST = API_BASE_URL+"thread/hottest";
+const fs = require('fs');
+function doGenerate(){
+    term('\n').green("Generating Users!");
+    //const userModel = require('./application/models/user');
+    //console.log(userModel);
 
-function doImport(){
-    term('\n').green("Do Import ! ");
+   /* mongoose.connect(`mongodb://mongo`, {
+        user: process.env.MONGODB_USER,
+        pass: process.env.MONGODB_PASS,
+        dbName: process.env.MONGODB_NAME,
+        useNewUrlParser: true,
+    });*/
 
-    terminate();
+   /* const db = mongoose.connection;
+
+    db.on('error', function () {
+        throw new Error('Connection failed');
+        terminate();
+    });
+
+    db.on('open', function () {
+        console.log('OKAYYY');
+        terminate();
+    });*/
+
 }
 
 function terminate() {
@@ -19,12 +44,12 @@ term.on( 'key' , function( name , matches , data ) {
 
 
 term.blue("What should i do ?");
-var items = ['import','dump','update','cancel'];
+var items = ['generate','dump','update','cancel'];
 
 term.gridMenu( items , function( error , response ) {
     switch (response.selectedText) {
-        case "import":
-            doImport();
+        case "generate":
+            doGenerate();
             break;
         default:
             term('\n').eraseLineAfter.blue("Goodbye");
