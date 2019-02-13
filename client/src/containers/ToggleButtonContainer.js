7929fc9b3eb0bf4499db2b997ba17ec884fc1620
AxelE <axel91evrard@gmail.com>
2019-02-13 16:30:15 +0100

    Ajout de components et containers

---
+++ client/src/containers/ToggleButtonContainer.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Banner from "../components/Banner";
+import ToggleButton from "../components/ToggleButton";
+
+export default class ToggleButtonContainer extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            theme: "black"
+        };
+    }
+
+    toggleTheme = () => {
+        console.log(this);
+        this.setState({ theme: this.state.theme === "black" ? "light" : "black" });
+    };
+
+    render() {
+        return (
+            <React.Fragment>
+                <ToggleButton toggleHandler={this.toggleTheme}/>
+                <Banner content={this.state.theme}/>
+            </React.Fragment>
+        )
+    }
+}
