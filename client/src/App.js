0abf35fccf779cba8fa06400bb6fb52b003298ef
AxelE <axel91evrard@gmail.com>
2019-02-14 15:39:50 +0100

    add Redux, todo login_check on server

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter, Link, Route } from "react-router-dom";
+import { BrowserRouter, Link, Route, Switch } from "react-router-dom";
 import ToggleButtonContainer from "./containers/ToggleButtonContainer";
 import SecurityContainer from "./containers/SecurityContainer";
 import ProductContainer from "./containers/ProductContainer";
+import ProfileBanner from "./containers/ProfileBanner"
 
 class App extends Component {
 
@@ -23,14 +24,16 @@ class App extends Component {
                     </p>
 
                     <ToggleButtonContainer/>
-
+                    <ProfileBanner/>
                     <BrowserRouter>
                         <React.Fragment>
-                            <Link to="/security">Security</Link>
+                            <Link to="/security/login">Login</Link>
                             <Link to="/products">Products</Link>
 
-                            <Route path="/security" component={SecurityContainer}/>
-                            <Route path="/products" component={ProductContainer}/>
+                            <Switch>
+                                <Route path="/security" component={SecurityContainer}/>
+                                <Route path="/products" component={ProductContainer}/>
+                            </Switch>
                         </React.Fragment>
                     </BrowserRouter>
 
--- client/src/containers/SecurityContainer.js
+++ client/src/containers/SecurityContainer.js
@@ -1,11 +1,28 @@
 import React from 'react';
+import { Route, Switch } from "react-router";
+import LoginForm from "./LoginForm";
+import {connect} from "react-redux";
+import {login} from "../redux/actions/security"
 
-export default class SecurityContainer extends React.Component {
+class SecurityContainer extends React.Component {
+    handleSubmit = (data) => {
+        //this.props.dispatch(login(data.username, data.password, this.props.dispatch));
+        this.props.login(data.username, data.password);
+    }
 
     render() {
         return (
-            <p>Security</p>
+            <Switch>
+                <Route path="/security/login" render={() => <LoginForm onSubmit={this.handleSubmit}/>}/>
+            </Switch>
         );
     }
-
 }
+
+const mapDispatchToProps = dispatch => {
+    return {
+        login: (username, password) => dispatch(login(username, password, dispatch))
+    }
+}
+
+export default connect(undefined, mapDispatchToProps)(SecurityContainer);
